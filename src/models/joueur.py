0344809c3f11bc28b503894f87d1bd9015f4cbde
Mickael Fioretti <mickael.fioretti@drotek.com>
2023-01-02 11:45:02 +0100

    [add-player] Joueur model with enum pydantic

--- src/models/joueur.py
+++ src/models/joueur.py
@@ -1,51 +1,20 @@
-from dataclasses import dataclass
+from pydantic.dataclasses import dataclass
+from enum import StrEnum
 
 
-class SexeError(Exception):
-    """Exception levée si le sexe n'est pas M ou F"""
+class Sexe(StrEnum):
+    """Enum représentant le sexe d'un joueur"""
 
-    pass
-
-
-class RankingError(Exception):
-    """Exception levée si le sexe n'est pas M ou F"""
-
-    pass
+    M = "M"
+    F = "F"
 
 
-MINIMAL_RANKING_VALUE = 0
-MAXIMAL_RANKING_VALUE = 1000
-
-
-@dataclass()
+@dataclass
 class Joueur:
-    """Classe représentant un joueur de d'échecs"""
+    """Classe représentant un joueur d'échecs"""
 
     first_name: str
     last_name: str
-    birth_date: str  # format JJ/MM/AAAA
-    sexe: str  # M ou F
-    ranking: int = 0  # 0 à 1000
-
-    def __post_init__(self):
-        """Méthode appelée après l'initialisation d'un joueur"""
-        if self.sexe not in ["M", "F"]:
-            raise SexeError("Le sexe doit être M ou F")
-        if self.ranking < MINIMAL_RANKING_VALUE or self.ranking > MAXIMAL_RANKING_VALUE:
-            raise RankingError("Le ranking doit être compris entre 0 et 1000")
-
-    def __str__(self):
-        """Méthode affichant les informations d'un joueur"""
-        return f"""
-                Nom : {self.last_name}\n
-                Prénom : {self.first_name}\n
-                Date de naissance : {self.birth_date}\n
-                Sexe : {self.sexe}\n
-                Ranking : {self.ranking}
-            """
-
-    def win(self) -> None:
-        self.ranking += 1
-
-    def __repr__(self):
-        return str(self)
+    birth_date: str
+    sexe: Sexe
+    ranking: int = 0
