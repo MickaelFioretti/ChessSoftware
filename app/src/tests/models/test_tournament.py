7fc65f4cd995ee6684c81c7c81d8d7c5b7944211
Mickael Fioretti <fiorettimickael1@gmail.com>
2023-04-06 10:22:55 +0200

    Installer pytest

--- app/src/tests/models/test_tournament.py
+++ app/src/tests/models/test_tournament.py
@@ -1,1 +1,26 @@
 import pytest
+
+from models.tournament import Tournament, Player
+
+@pytest.fixture
+def tournament():
+    # Fixture pour initialiser le tournoi avec 4 joueurs
+    tournament = Tournament()
+    players = [
+        Player(first_name="Jean", name="Dupont", birth_date="20/03/2023"),
+        Player(first_name="Fioretti", name="Mickael", birth_date="20/03/2023"),
+        Player(first_name="Julien", name="Levieux", birth_date="20/02/2020"),
+        Player(first_name="Tony", name="Stark", birth_date="20/02/2020")
+    ]
+    tournament.add_players(players)
+    return tournament
+
+
+def test_create_players_pairs(tournament):
+    # Test pour vérifier que la fonction crée des paires correctes pour le premier tour
+    pairs = tournament.create_players_pairs(current_round=0)
+    assert len(pairs) == 2
+    assert pairs[0][0].name == "Dupont"
+    assert pairs[0][1].name == "Levieux"
+    assert pairs[1][0].name == "Stark"
+    assert pairs[1][1].name == "Mickael"
