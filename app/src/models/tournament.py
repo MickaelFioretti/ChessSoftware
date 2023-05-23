e2722ad01302f81333bf5297cf704c1777d26970
Mickael Fioretti <mickael.fioretti@drotek.com>
2023-05-23 11:30:09 +0200

    Fix : player pair but some little bug on save data

--- app/src/models/tournament.py
+++ app/src/models/tournament.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 import dataclasses
 from .round import Round
 from .player import Player
+import random
 
 # flake8: noqa
 
@@ -31,52 +32,37 @@ class Tournament:
         """Method creating pairs of players"""
         players_pairs = []
 
-        # --- First round sort player by rank ---
+        # --- First round ---
         if current_round == 0:
-            sorted_players = sorted(self.players, key=lambda x: x.ranking, reverse=True)
-
-        # --- Next rounds sort player by score ---
+            # --- Shuffle players ---
+            players = self.players.copy()
+            random.shuffle(players)
+        # --- Other rounds ---
         else:
-            sorted_players = []
-            score_sorted_players = sorted(
-                self.players, key=lambda x: x.total_score, reverse=True
-            )
-
-            # --- Sort by rank if same score ---
-            for i in range(len(score_sorted_players)):
-                player = score_sorted_players[i]
-                sorted_players.append(player)
-                if (
-                    i < len(score_sorted_players) - 1
-                    and player.total_score == score_sorted_players[i + 1].total_score
-                ):
-                    if player.ranking < score_sorted_players[i + 1].ranking:
-                        hi_player = player
-                        lo_player = score_sorted_players[i + 1]
-                    else:
-                        hi_player = score_sorted_players[i + 1]
-                        lo_player = player
-                    sorted_players.append(hi_player)
-                    sorted_players.append(lo_player)
-
+            players = sorted(self.players, key=lambda x: x.ranking, reverse=False)
+        
         # --- Create pairs ---
-        # Split players in two parts
-        half_len = len(sorted_players) // 2
-        sup_part = sorted_players[half_len:]
-        inf_part = sorted_players[:half_len]
+        # Split players list in two
+        half_len = len(players) // 2
+        sup_part = players[:half_len]
+        inf_part = players[half_len:]
 
         # Create pairs
-        for i, player in enumerate(sup_part):
-            for player2 in inf_part:
-                if player2 not in player.played_with:
-                    players_pairs.append([player, player2])
-                    player.played_with.append(player2)
-                    player2.played_with.append(player)
-                    break
+        # if players have already played together, switch the second player
+        # in played_with compared with the first_name and name
+        # and if players have the same score, switch the second player
+        # at the end add first_name and name to played_with
+        for i in range(half_len):
+            if inf_part[i].first_name + inf_part[i].name in sup_part[i].played_with:
+                if inf_part[i].ranking == sup_part[i].ranking:
+                    players_pairs.append([sup_part[i], inf_part[i + 1]])
+                    sup_part[i].played_with.append(f"{inf_part[i + 1].first_name} {inf_part[i + 1].name}")
+                    inf_part[i + 1].played_with.append(f"{sup_part[i].first_name} {sup_part[i].name}")
             else:
-                players_pairs.append([player, inf_part[i % half_len]])
-                player.played_with.append(inf_part[i % half_len])
-                inf_part[i % half_len].played_with.append(player)
+                players_pairs.append([sup_part[i], inf_part[i]])
+                sup_part[i].played_with.append(f"{inf_part[i].first_name} {inf_part[i].name}")
+                inf_part[i].played_with.append(f"{sup_part[i].first_name} {sup_part[i].name}")
+
         return players_pairs
 
     def get_ranking(self, by_score=False):
--- app/src/tests/models/test_tournament.py
+++ app/src/tests/models/test_tournament.py
@@ -18,25 +18,29 @@ def tournament():
                 first_name="Mickael",
                 name="Stark",
                 birth_date="01/01/1980",
-                ranking=0,
+                ranking=1,
+                played_with=[],
             ),
             Player(
                 first_name="Jean",
                 name="Dupont",
                 birth_date="01/01/1980",
-                ranking=0,
+                ranking=3,
+                played_with=[],
             ),
             Player(
                 first_name="Paul",
                 name="Levieux",
                 birth_date="01/01/1980",
-                ranking=0,
+                ranking=4,
+                played_with=[],
             ),
             Player(
                 first_name="Jacques",
-                name="Dupont",
+                name="Lefou",
                 birth_date="01/01/1980",
-                ranking=0,
+                ranking=2,
+                played_with=[],
             ),
         ]
     )
@@ -48,9 +52,10 @@ def test_create_players_pairs(tournament):
     """Test pour vérifier que la fonction crée des paires
     correctes pour le premier tour"""
 
-    pairs = tournament.create_players_pairs(current_round=0)
+    pairs = tournament.create_players_pairs(current_round=1)
 
-    # Test aue les paire sont bien créées et que chaque joueur ne joue qu'une fois
+    # Test que les paire sont bien créées et que
+    # chaque joueur ne joue qu'une fois
     assert len(pairs) == 2
     assert len(pairs[0]) == 2
     assert len(pairs[1]) == 2
@@ -60,3 +65,13 @@ def test_create_players_pairs(tournament):
     assert pairs[0][0] != pairs[1][1]
     assert pairs[0][1] != pairs[1][0]
     assert pairs[0][1] != pairs[1][1]
+    
+    # Test que les paires sont bien créées en fonction du classement
+    assert pairs[0][0].ranking < pairs[0][1].ranking
+    assert pairs[1][0].ranking < pairs[1][1].ranking
+    
+    # Test que le played_with est bien mis à jour
+    assert pairs[0][0].played_with == [f"{pairs[0][1].first_name} {pairs[0][1].name}"]
+    assert pairs[0][1].played_with == [f"{pairs[0][0].first_name} {pairs[0][0].name}"]
+    assert pairs[1][0].played_with == [f"{pairs[1][1].first_name} {pairs[1][1].name}"]
+    assert pairs[1][1].played_with == [f"{pairs[1][0].first_name} {pairs[1][0].name}"]
